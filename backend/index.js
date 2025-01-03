cb3c9c298b5acdc13aa8f51c1f47af8488248681
raunak123455 <91775838+raunak123455@users.noreply.github.com>
2025-01-03 14:27:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -13,11 +13,19 @@ const PORT = process.env.PORT || 8080;
 const URL = process.env.DATABASE_URL;
 app.use(express.json());
 
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://task-finall.vercel.app"], // Add your frontend domains
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true, // Enable credentials (cookies, authorization headers, etc)
+  optionsSuccessStatus: 200,
+};
+
 app.get("/", (req, res) => {
   res.send("App working lad");
 });
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 app.use("/api/user/", router);
